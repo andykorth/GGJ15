3ece3da7f912f332a453d0f957521afb545dc98b
Andy Korth <andy@howlingmoonsoftware.com>
2015-01-23 17:06:39 -0600

    Git submodules and scene changes

--- Source/MainScene.h
+++ Source/MainScene.h
@@ -1,3 +1,8 @@
+
+#import "cocos2d.h"
+
 @interface MainScene : CCNode
+
+@property (nonatomic,strong) CCLabelTTF* title;
 
 @end
--- Source/MainScene.m
+++ Source/MainScene.m
@@ -2,4 +2,26 @@
 
 @implementation MainScene
 
+- (void) didLoadFromCCB
+{
+    [self setUserInteractionEnabled:true];
+    _title.string = @"No key";
+}
+
+
+-(void)update:(CCTime)delta
+{
+    
+}
+
+- (void)keyDown:(NSEvent *)theEvent
+{
+    _title.string = [NSString stringWithFormat:@"KeyDown: %@", theEvent.characters];
+}
+
+- (void)keyUp:(NSEvent *)theEvent
+{
+    _title.string = [NSString stringWithFormat:@"KeyUp: %@", theEvent.characters];
+}
+
 @end
