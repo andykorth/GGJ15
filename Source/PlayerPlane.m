95e2be0cc2514f196f0da34789d728022add4417
slembcke <slembcke@gmail.com>
2015-01-24 03:14:59 -0600

    More airplane-like physics.

--- Source/PlayerPlane.m
+++ Source/PlayerPlane.m
@@ -1,5 +1,5 @@
 #import "PlayerPlane.h"
-#import "ObjectiveChipmunk/ObjectiveChipmunk.h"
+#import "CCPhysics+ObjectiveChipmunk.h"
 
 @implementation PlayerPlane{
 
@@ -27,24 +27,55 @@
 
 - (void) update:(CCTime)delta
 {
+    const cpFloat forwardAcceleration = 600.0;
+    const cpFloat maxForwardSpeed = 400.0;
+    
+    // Maximum rotation speed when traveling at the maximum speed.
+    const cpFloat maxRotationSpeed = 5.0;
+    
+    CCPhysicsBody *body = self.physicsBody;
+    cpTransform transform = body.body.transform;
+//        cpVect forward = cpv(-transform.a, -transform.b);
+//        cpVect up = cpv(transform.c, transform.d);
+    
+    // Stupid hack to flip the transform since the ship points against the x-axis.
+    transform.a = -transform.a;
+    transform.b = -transform.b;
+    
+    // Get the local velocity. (+x is forward, +y is up)
+    cpVect velocity = cpTransformVect(cpTransformInverse(transform), body.velocity);
+    
+    // Apply small amount of forward drag, lots of lateral drag.
+    cpVect drag = cpv(0.95, 0.2);
+    velocity.x *= pow(drag.x, delta);
+    velocity.y *= pow(drag.y, delta);
+    
     if(_keyDowns[_playerNumber == 0 ? @(13) : @(126)]){ //w
-        GLKVector4 forward = GLKMatrix4MultiplyVector4(self.nodeToParentMatrix, GLKVector4Make(-1, 0, 0, 0));
+        // Apply simple forward acceleration.
+        velocity.x = cpflerpconst(velocity.x, maxForwardSpeed, forwardAcceleration*delta);
         
-        cpFloat acceleration = 500.0;
-        cpVect desiredVelocity = cpvmult(cpv(forward.x, forward.y), 40.0);
-        
-        self.physicsBody.velocity = cpvlerpconst(self.physicsBody.velocity, desiredVelocity, acceleration*delta);
     }
+//    if(_keyDowns[_playerNumber == 0 ? @(1) : @(125)]){ //s
+//        // air brake?
+//    }
+    
+    self.physicsBody.velocity = cpTransformVect(transform, velocity);
+    
+    // Direction of rotation to apply to the player.
+    cpFloat rotation = 0.0;
+    
     if(_keyDowns[_playerNumber == 0 ? @(0) : @(123)]){ //a
-        self.rotation = self.rotation += delta * -200.0f;
-    }
-    if(_keyDowns[_playerNumber == 0 ? @(1) : @(125)]){ //s
-        // air brake?
+        rotation += 1.0;
     }
     if(_keyDowns[_playerNumber == 0 ? @(2) : @(124)]){ //d
-        self.rotation = self.rotation += delta * 200.0f;
+        rotation -= 1.0;
     }
     
+    // Plane turns better the faster you are going.
+    cpFloat controlCoefficient = fmax(fabs(velocity.x/maxForwardSpeed), 0.25);
+    
+    cpFloat rotationSpeed = maxRotationSpeed*rotation*controlCoefficient;
+    body.absoluteRadians += rotationSpeed*delta;
 }
 
 @end
