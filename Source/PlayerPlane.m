e58c780f8701b57063e114eea0402000f8a18dfb
Andy Korth <andy@howlingmoonsoftware.com>
2015-01-23 23:29:35 -0600

    sortakinda input

--- Source/PlayerPlane.m
+++ Source/PlayerPlane.m
@@ -3,6 +3,14 @@
 @implementation PlayerPlane
 
 
+
+
+- (void) didLoadFromCCB
+{
+    self.userInteractionEnabled= true;
+    self.physicsBody.allowsRotation = false;
+}
+
 - (void)keyDown:(NSEvent *)theEvent
 {
     NSLog(@"Key down: %@", theEvent);
@@ -10,7 +18,15 @@
 
 - (void)keyUp:(NSEvent *)theEvent
 {
+    GLKVector4 v = GLKMatrix4MultiplyVector4(self.nodeToParentMatrix, GLKVector4Make(1, 0, 0, 0));
+    
+    self.physicsBody.velocity = ccp(-v.x * 40.0f, -v.y * 40.0f);
+}
 
+- (void) update:(CCTime)delta
+{
+    
+    
 }
 
 @end
