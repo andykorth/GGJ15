e960b5ce6f5bcd2378c2a22c596c79825c04f338
slembcke <slembcke@gmail.com>
2015-01-24 12:23:50 -0600

    Bombs oops.

--- Source/Bomb.m
+++ Source/Bomb.m
@@ -1,13 +1,13 @@
-#import "Bullet.h"
+#import "Bomb.h"
 
-@implementation Bullet
+@implementation Bomb
 
 -(id)initWithGroup:(id)group;
 {
     if((self = [super init])){
         CCPhysicsBody *body = [CCPhysicsBody bodyWithCircleOfRadius:5.0 andCenter:CGPointZero];
         body.collisionGroup = group;
-        body.collisionType = @"bullet";
+        body.collisionType = @"bomb";
         
         self.physicsBody = body;
     }
@@ -18,7 +18,7 @@
 -(void)onEnter
 {
     [super onEnter];
-    [self scheduleBlock:^(CCTimer *timer){[self destroy];} delay:1.0];
+    [self scheduleBlock:^(CCTimer *timer){[self destroy];} delay:5.0];
 }
 
 -(void)destroy
