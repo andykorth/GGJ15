9550fec30c5ef5020699efb6e0684e0f5233a075
slembcke <slembcke@gmail.com>
2015-01-24 05:16:46 -0600

    Weird rotational drag.

--- Source/PlayerPlane.m
+++ Source/PlayerPlane.m
@@ -11,7 +11,7 @@
     _keyDowns = [NSMutableDictionary dictionary];
     
     self.userInteractionEnabled= true;
-    self.physicsBody.allowsRotation = false;
+//    self.physicsBody.allowsRotation = false;
 }
 
 - (void)keyDown:(NSEvent *)theEvent
@@ -33,6 +33,11 @@
     // Maximum rotation speed when traveling at the maximum speed.
     const cpFloat maxRotationSpeed = 5.0;
     
+    // Offset in points of the center of drag relative to the center of gravity.
+    const cpFloat codOffset = 5.0;
+    
+    const cpVect linearDrag = cpv(0.95, 0.1);
+    
     CCPhysicsBody *body = self.physicsBody;
     cpTransform transform = body.body.transform;
 //        cpVect forward = cpv(-transform.a, -transform.b);
@@ -46,9 +51,8 @@
     cpVect velocity = cpTransformVect(cpTransformInverse(transform), body.velocity);
     
     // Apply small amount of forward drag, lots of lateral drag.
-    cpVect drag = cpv(0.95, 0.2);
-    velocity.x *= pow(drag.x, delta);
-    velocity.y *= pow(drag.y, delta);
+    velocity.x *= pow(linearDrag.x, delta);
+    velocity.y *= pow(linearDrag.y, delta);
     
     if(_keyDowns[_playerNumber == 0 ? @(13) : @(126)]){ //w
         // Apply simple forward acceleration.
@@ -71,11 +75,22 @@
         rotation -= 1.0;
     }
     
-    // Plane turns better the faster you are going.
-    cpFloat controlCoefficient = fmax(fabs(velocity.x/maxForwardSpeed), 0.25);
+    // Now handle the rotational velocity.
+    cpFloat angularVelocity = body.angularVelocity;
     
-    cpFloat rotationSpeed = maxRotationSpeed*rotation*controlCoefficient;
-    body.absoluteRadians += rotationSpeed*delta;
+    // Plane turns better the faster you are going.
+    cpFloat controlCoefficient = fmax(fabs(velocity.x/maxForwardSpeed), 0.1);
+    cpFloat controlRotation = maxRotationSpeed*rotation*controlCoefficient;
+    
+    // Rotation due to the movement of the center of drag.
+    cpFloat dragRotation = -velocity.y/codOffset;
+    
+    cpFloat targetAngularVelocity = cpflerp(dragRotation, controlRotation, fabs(rotation));
+    body.angularVelocity = cpflerp(
+        cpflerp(dragRotation, angularVelocity, pow(0.8, delta)),
+        controlRotation,
+        fabs(rotation)
+    );
 }
 
 @end
